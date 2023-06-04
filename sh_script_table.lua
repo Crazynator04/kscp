05bb42b38c87db0004d50c4c5f0769a9c13d3bf7
Kyle <51022413+Crazynator04@users.noreply.github.com>
2023-06-04 13:35:45 +0200

    Create sh_script_table.lua

---
+++ sh_script_table.lua
@@ -0,0 +1,36 @@
+kSCP:RegisterScript("999", "SCP-999","TUaJ2ZI", "Become a slime ball which makes sounds and is able to heal other players. And he can decide on his own how big he wanna be!", {
+    {preset = "title1", str = "SCP-999 | The Slime", color = Color(193, 94, 13)},
+    {size = 28, weight = 700, str = "Features", color = Color(255, 255, 255)},
+    {size = 26, str = "- Grow on Rightclick! \nYou can grow up to 1.5 times your own size", color = Color(203, 203, 203)},
+    {size = 26, str = "- Shrink on Leftclick! \nYou can even become as small as a third of your original size. We'll even update your POV and hitbox", color = Color(203, 203, 203)},
+    {size = 26, str = "- Heal others on Reload! \nYou are a small slime ball, of course you can also help your beloved foundation personal or scps and heal them!", color = Color(203, 203, 203)},
+    {size = 26, str = "- No sound spaming anymore!\nSCP-999 makes every 50 seconds (you can edit this in a config) one of 7 different sounds! Wanna hear them? You can check 'em out right now!", color = Color(203, 203, 203)},
+    {size = 16, str = "- This will only work if you have the script already installed on your client!", color = Color(203, 203, 203)},
+    {typ = "sound", str = "kscp/999/scp_999_1.wav"}, {typ = "sound", str = "kscp/999/scp_999_2.wav"}, {typ = "sound", str = "kscp/999/scp_999_3.wav"}, {typ = "sound", str = "kscp/999/scp_999_4.wav"},{typ = "sound", str = "kscp/999/scp_999_5.wav"},{typ = "sound", str = "kscp/999/scp_999_6.wav"},{typ = "sound", str = "kscp/999/scp_999_7.wav"},
+}, "workshop", "github")
+
+kSCP:RegisterScript("073", "SCP-073", "gxbt4iW", "Always wanted to become invincible? Now you can play SCP 073, other known as Cain. Every damage he takes won't affect him but the person who tried to hurt him.", {
+    {preset = "title1", str = "SCP 073 | Cain", color = Color(43, 147, 173)},
+    {size = 28, weight = 700, str = "Features", color = Color(255, 255, 255)},
+    {size = 26, str = "- Blocks every damage taken and reflects it on the player or npc who attacked you", color = Color(203, 203, 203)},
+    {size = 26, str = "- The swep only needs to be in your inventory and it already works", color = Color(203, 203, 203)},
+    {size = 26, str = "- It shows Cain how much damage he gave to this attacking person", color = Color(203, 203, 203)},
+    {size = 26, str = "- There won't be any blood particles if you hit Cain", color = Color(203, 203, 203)},
+})
+
+kSCP:RegisterScript("966", "SCP-966", "nwFInDy", "Becoming invisible and hunting people in their dreams sounds fun? You can right go on then!", {
+    {preset = "title1", str = "SCP-966 | Sleep Killer"},
+    {size = 28, weight = 700, str = "Features", color = Color(255, 255, 255)},
+    {size = 26, str = "- Become invisible for everybody except your fellow 966 instances", color = Color(203, 203, 203)},
+    {size = 26, str = "- Night Goggles Swep which allows you to see 966 instances\nIt even works in the dark etc., so you can use it for far more occasions than just hunting 966 down!", color = Color(203, 203, 203)},
+    {size = 26, str = "- From time to time 966 emits a scary sound, just like in SCP:CB", color = Color(203, 203, 203)},
+    {size = 26, str = "- Right and left click to slice your enemies", color = Color(203, 203, 203)},
+})
+
+--[[kSCP:RegisterScript("disallow_voice", "Mute SCPs", "XipsQCY", "SCPs are trying to talk and that's getting on your nerves? No worries! With this script you can block any DarkRP team from talking")
+
+kSCP:RegisterScript("939", "SCP-939", "xDoItBc", "Always wanted to hunt your enemys down like a dog? Well, now you have the chance to! Become a big red dog and kill whoever crosses your way!")
+
+kSCP:RegisterScript("268", "SCP-268", "Uuvl7dg", "Becoming invisible is something you like? Then download me!")
+
+kSCP:RegisterScript("650", "SCP-650", "ZrM4QGa", "A scary statue which is following you. And it's not 173")]]--
